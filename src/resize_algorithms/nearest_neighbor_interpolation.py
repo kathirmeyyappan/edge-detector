592e068635dda88e2dcbafc3cd51b0c089510f8a
kathirmeyyappan <kathirmey@gmail.com>
2023-06-05 19:02:26 -0400

    resizing works

--- src/resize_algorithms/nearest_neighbor_interpolation.py
+++ src/resize_algorithms/nearest_neighbor_interpolation.py
@@ -20,25 +20,41 @@ def nearest_neighbor_interpolation(img_arr: np.ndarray, resize_factor: float) ->
     Returns:
         np.ndarray: Array representation of the blurred image
     """
-    raise NotImplementedError
+    h, w, _ = img_arr.shape
+    
+    # resizing new image array based on scale
+    new_h, new_w = int(h * resize_factor), int(w * resize_factor)
+    new_img_arr = np.zeros((new_h, new_w, 3), dtype=np.uint8)
+    
+    # finding corresponding point in scalar interpolation from old image
+    # to assign in new image
+    for y, row in enumerate(new_img_arr):
+        y_corr = int(y / new_h * h)
+        for x, _ in enumerate(row):
+            x_corr = int(x / new_w * w)
+            new_img_arr[y, x] = img_arr[y_corr, x_corr]
+    
+    return new_img_arr
+            
 
 # click commands
 @click.command(name="nearest_neighbor_interpolation")
 @click.option('-f', '--filename', type=click.Path(exists=True))
 @click.option('-s', '--resize_factor', type=float, default=2.00)
 
-def blur(filename: str, resize_factor: float) -> None:
+def resize(filename: str, resize_factor: float) -> None:
     """
     command
     """
-    if float <= 0:
+    if resize_factor <= 0:
         raise ValueError("size factor must be positive")
     
     with Image.open(filename) as img:
         img_arr = np.array(img)
         new_img_arr = nearest_neighbor_interpolation(img_arr, resize_factor)
+        print(type(new_img_arr), new_img_arr.shape)
         new_img = Image.fromarray(new_img_arr)
         new_img.show()
 
 if __name__ == "__main__":
-    blur()
+    resize()
