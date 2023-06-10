465f674cb5f14763e20071dcf1002893e524113b
kathirmeyyappan <kathirmey@gmail.com>
2023-06-10 17:44:52 -0400

    sobel edge detection works (need to do arctan)

--- src/edge_detection_algorithms/canny_edge_detector.py
+++ src/edge_detection_algorithms/canny_edge_detector.py
@@ -21,25 +21,69 @@ def canny_edge_detection(img_arr: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: Array representation of the edge detector applied image
     """
+    # 
+    HEIGHT, WIDTH, _ = img_arr.shape
+    
     
     # GREYSCALING IMAGE
+    
     greyscaled_img_arr = greyscale(img_arr)
     print("IMAGE GREYSCALED")
     time.sleep(1)
     
+    
     # PERFORMING NOISE REDUCTION WITH MEDIAN GAUSSIAN BLUR
-    # median blur, radius = 1
-    median_blurred_img_arr = median_blur(greyscaled_img_arr, radius=1)
+    
+    # median blur, radius depends on image size to clear 
+    r = 1 if WIDTH < 500 else 2
+    median_blurred_img_arr = median_blur(greyscaled_img_arr, radius=r)
     print("IMAGE MEDIAN BLURRED")
     time.sleep(1)
+    
     # Gaussian blur, sigma = 1
     noise_reduced_img_arr = gaussian_blur(median_blurred_img_arr, sigma=1)
     print("IMAGE GAUSSIAN BLURRED")
     time.sleep(1)
     
-    # NEXT STEP
     
-    return noise_reduced_img_arr
+    # GRADIENT CALCULATION
+    
+    # turning image array into 2-d array
+    intensity_arr = noise_reduced_img_arr[:, :, 0]
+    
+    # X and Y Sobel filters (discrete derivative approximations in dx and dy)
+    X_KERNEL = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    Y_KERNEL = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+    
+    # creating array to be convolved with derivative gradients
+    convolved_arr = intensity_arr.copy()
+    
+    # iterating through intensity_arr to fill convolved array
+    for y, row in enumerate(intensity_arr):
+        # ignore edge
+        if y == 0 or y == HEIGHT - 1:
+            continue
+        
+        for x, _ in enumerate(row):
+            # ignore edge
+            if x == 0 or x == WIDTH - 1:
+                continue
+            
+            # get Hadamard product and array sum to calculate intensity change
+            g_x = np.sum(X_KERNEL * intensity_arr[y-1:y+2, x-1:x+2])
+            g_y = np.sum(Y_KERNEL * intensity_arr[y-1:y+2, x-1:x+2])
+            
+            convolved_arr[y,x] = np.sqrt(g_x**2 + g_y**2)
+    
+    convolved_arr = np.squeeze(convolved_arr)
+    
+    final_image = np.zeros((HEIGHT - 1, WIDTH - 1, 3), dtype=np.uint8)
+    for y, row in enumerate(final_image):        
+        for x, g in enumerate(row):
+            val = convolved_arr[y,x]
+            final_image[y, x] = np.array([val, val, val])
+    
+    return final_image
 
 
 # click commands
