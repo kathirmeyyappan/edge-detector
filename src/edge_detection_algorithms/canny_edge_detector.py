6e357507a7176c9136ca50347061404e121ad708
kathirmeyyappan <kathirmey@gmail.com>
2023-06-11 11:47:34 -0400

    non-maximum supression working

--- src/edge_detection_algorithms/canny_edge_detector.py
+++ src/edge_detection_algorithms/canny_edge_detector.py
@@ -1,7 +1,7 @@
 """
 This is my implementation of the Canny edge detection algorithm. Note that the
 first 3 steps bear striking similarity to the Sobel edge detection method with
-the exception of the calculation of theta, which we do use here later on.
+the exception of the calculation of theta, which we use here later on.
 """
 
 from typing import List, Tuple, Optional
@@ -23,17 +23,20 @@ def canny_edge_detection(img_arr: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: Array representation of the edge detector applied image
     """
-    # 
+    # image array dimensions
     HEIGHT, WIDTH, _ = img_arr.shape
     
     
-    # GREYSCALING IMAGE
+    ### GREYSCALING IMAGE ###
+    
+    
     greyscaled_img_arr = greyscale(img_arr)
     print("IMAGE GREYSCALED")
     time.sleep(1)
     
     
-    # PERFORMING NOISE REDUCTION WITH MEDIAN AND GAUSSIAN BLUR
+    ### PERFORMING NOISE REDUCTION WITH MEDIAN AND GAUSSIAN BLUR ###
+    
     # median blur, radius depends on image size to clear 
     r = 0 if WIDTH < 500 else 1
     median_blurred_img_arr = median_blur(greyscaled_img_arr, radius=r)
@@ -41,12 +44,14 @@ def canny_edge_detection(img_arr: np.ndarray) -> np.ndarray:
     time.sleep(1)
     
     # Gaussian blur, sigma = 1
-    noise_reduced_img_arr = gaussian_blur(median_blurred_img_arr, sigma=2)
+    s = 1 if WIDTH < 500 else 2
+    noise_reduced_img_arr = gaussian_blur(median_blurred_img_arr, sigma=s)
     print("IMAGE GAUSSIAN BLURRED")
     time.sleep(1)
     
     
-    # GRADIENT CALCULATION
+    ### GRADIENT CALCULATION ###
+    
     # turning image array into 2-d array (shows only intensity)
     intensity_arr = noise_reduced_img_arr[:, :, 0]
     
@@ -59,9 +64,9 @@ def canny_edge_detection(img_arr: np.ndarray) -> np.ndarray:
                          [1, 2, 1]])
     
     # creating array to be convolved with estimated derivative gradients
-    convolved_arr = intensity_arr.copy()
+    convolved_arr = np.zeros((HEIGHT, WIDTH))
     # creating array to store theta (direction) values for intensity change
-    theta_vals = intensity_arr.copy()
+    theta_vals = np.zeros((HEIGHT, WIDTH))
     
     # iterating through intensity_arr to fill convolved array
     for y, row in enumerate(intensity_arr):
@@ -81,22 +86,79 @@ def canny_edge_detection(img_arr: np.ndarray) -> np.ndarray:
             # assignment of intensity change value (from gradient)
             convolved_arr[y, x] = np.sqrt(g_x**2 + g_y**2)
             # specialized arctan function to store direction of gradient change
-            theta_vals[y, x] = np.arctan2(g_x, g_y)
-            
+            theta_vals[y, x] = np.arctan2(g_y, g_x)
+
+    # removing edges that weren't calculated
+    convolved_arr = np.squeeze(convolved_arr)
+    theta_vals = np.squeeze(theta_vals)
+    
+    # redirecting theta values to lie only between 0 and 180 (mainting direction)
+    theta_vals = theta_vals.astype(np.float64)
+    theta_vals[theta_vals < 0] += np.pi
+          
     print("GRADIENT AND IMAGE ARRAY CONVOLVED\nTHETA VALUES CALCULATED")
     time.sleep(1)
     
-    # removing edges that weren't calculated
-    convolved_arr = np.squeeze(convolved_arr)
     
+    ### APPLYING NON-MAXIMUM SUPRESSION ###
     
+    # creating new array to store values from non-maximum suppression
+    suppressed_arr = convolved_arr.copy()
     
-    # creating final image array 
+    # iterating through convolved_arr to find perpendicular points and determine
+    # if point is valid edge according to theta and adjacent intensities
+    for y, row in enumerate(convolved_arr):
+        # ignore edge
+        if y == 0 or y == HEIGHT - 1:
+            continue
+        if y % 10 == 0:
+            print(f"{y}/{HEIGHT - 1} pixel rows calculated for "
+                  "non-maximum supression")
+            
+        for x, _ in enumerate(row):
+            # ignore edge
+            if x == 0 or x == WIDTH - 1:
+                continue
+                
+            
+            # finding which adjacent angles to check using adjusted theta
+            theta = theta_vals[y, x]
+            
+            if 0 <= theta < np.pi / 8 or \
+            7 * np.pi / 8 <= theta < np.pi:
+                adj_1 = convolved_arr[y, x - 1]
+                adj_2 = convolved_arr[y, x + 1]
+            
+            elif np.pi / 8 <= theta <= 3 * np.pi / 8:
+                adj_1 = convolved_arr[y - 1, x + 1]
+                adj_2 = convolved_arr[y + 1, x - 1]
+            
+            elif 3 * np.pi / 8 <= theta <= 5 * np.pi / 8:
+                adj_1 = convolved_arr[y - 1, x]
+                adj_2 = convolved_arr[y + 1, x]
+            
+            elif 5 * np.pi / 8 <= theta <= 7 * np.pi / 8:
+                adj_1 = convolved_arr[y + 1, x + 1]
+                adj_2 = convolved_arr[y - 1, x - 1]
+                
+            # assigning new values to suppressed array if center intensity is
+            # larger than neighbors (otherwise 0)
+            if convolved_arr[y, x] >= adj_1 and convolved_arr[y, x] >= adj_2:
+                suppressed_arr[y, x] = convolved_arr[y, x]
+            else:
+                suppressed_arr[y, x] = 0
+            
+    print("NON-MAXIMUM SUPPRESSION APPLIED")
+    time.sleep(1)     
+            
+    
+    ### RETURNING FINAL IMAGE ARRAY ###
+    
     final_image = np.zeros((HEIGHT - 1, WIDTH - 1, 3), dtype=np.uint8)
-    # putting intensity values from convolved array back into RGB format
+    # putting intensity values from final array back into RGB format
     for y, row in enumerate(final_image):        
-        for x, g in enumerate(row):
-            val = convolved_arr[y,x]
+        for x, _ in enumerate(row):
+            val = suppressed_arr[y, x]
             final_image[y, x] = np.array([val, val, val])
     
     print("DONE")
